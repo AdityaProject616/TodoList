f9701e8db483616006d9663000fb130cfb400fcc
Aditya Muthal <muthaladitya7@gmail.com>
2021-08-26 08:53:05 +0530

    Change Port and favicon

--- app.js
+++ app.js
@@ -125,6 +125,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started ");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});                                           
+
